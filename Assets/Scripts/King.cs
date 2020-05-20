81d24ded7945ed75c4278a6c8e68dfed1dcf7974
wladweb <wladwebwork@gmail.com>
2020-05-20 14:03:37 +0300

    added all kings animations

---
+++ Assets/Scripts/King.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class King : MonoBehaviour
+{
+    [SerializeField] private int _health;
+    [SerializeField] private float _speed;
+
+    private Animator _animator;
+    private Rigidbody2D _rigidBody;
+}
